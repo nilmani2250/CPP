09f25fd8234d2fac859b422f8c520db2e31878d3
Ravi Gautam Nimani <111082370+nilmani2250@users.noreply.github.com>
2025-02-19 18:46:27 +0530

    Update Non_static_datia_member.cpp

--- Non_static_datia_member.cpp
+++ Non_static_datia_member.cpp
@@ -1,12 +1,10 @@
 #include<iostream>
-
 class Car
 {
 	private :
 		float fuel{10.5};
 		int speed{0};
 		int passenger{0};
-
 	public :
 		Car()
 		{
@@ -24,7 +22,6 @@ int main()
 {
 	Car CAR;
 	CAR.display();
-
 	return 0;
 }
 
