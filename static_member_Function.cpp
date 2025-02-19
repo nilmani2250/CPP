b1d1a3c4519c97778bdfd0baf5e0c509db898cc5
Ravi Gautam Nimani <111082370+nilmani2250@users.noreply.github.com>
2025-02-19 18:47:29 +0530

    Add files via upload

---
+++ static_member_Function.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+class Car
+{
+        private:
+                float fuel;
+                int speed;
+                int passenger;
+
+                static int totalcar;
+
+        public:
+                // Constructor
+                Car()
+                {
+                        fuel = 10.5;
+                        speed = 0;
+                        passenger = 1;
+
+                        totalcar++;
+                        std::cout << "Created Car: " << totalcar << std::endl;
+                }
+
+                // Destructor
+                ~Car()
+                {
+                        totalcar--;
+                        std::cout << "Destroyed: " << totalcar << std::endl;
+                }
+
+
+                // Static member function 
+                static void Totalcars()
+                {
+            
+                        std::cout << "TOTAL CAR: " << totalcar << std::endl;
+                }
+};
+
+// Initialize static member variable
+int Car::totalcar = 0;
+
+int main()
+{
+        Car car1;
+        Car car2;
+
+	 Car::Totalcars();
+
+        return 0;
+}
+            
